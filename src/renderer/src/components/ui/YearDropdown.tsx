bd569a569fef323c07ac8d0b2ea86d78afa26cd4
Leathermaker <leatherwebmaker123@gmail.com>
2025-03-21 18:31:08 +0530

    add year filter

--- src/renderer/src/components/ui/YearDropdown.tsx
+++ src/renderer/src/components/ui/YearDropdown.tsx
@@ -1,19 +1,22 @@
-import { useRef, useState } from "react";
-import { ChevronDown, ChevronUp } from "lucide-react";
+import { useRef, useState } from 'react'
+import { ChevronDown, ChevronUp } from 'lucide-react'
+import useYearFilterStore from '@renderer/state_manager/yearFilter'
+import {motion} from 'motion/react'
 
 const YearDropdown = () => {
-  const [isOpen, setIsOpen] = useState(false);
-  const [selectedYear, setSelectedYear] = useState("Select Year");
-  const YearDropDownRef = useRef<HTMLUListElement | null>(null);
-  const currentYear = new Date().getFullYear();
-  const years = Array.from({ length: currentYear - 2009 }, (_, i) => 2010 + i);
-
-  const toggleDropdown = () => setIsOpen(!isOpen);
+  const [isOpen, setIsOpen] = useState(false)
+  const [selectedYear, setSelectedYear] = useState('Select Year')
+  const YearDropDownRef = useRef<HTMLUListElement | null>(null)
+  const currentYear = new Date().getFullYear()
+  const years = Array.from({ length: currentYear - 2009 }, (_, i) => 2010 + i)
+  const { setYear } = useYearFilterStore()
+  const toggleDropdown = () => setIsOpen(!isOpen)
 
   const handleSelect = (year: number) => {
-    setSelectedYear(JSON.stringify(year));
-    setIsOpen(false);
-  };
+    setSelectedYear(JSON.stringify(year))
+    setYear(year)
+    setIsOpen(false)
+  }
 
   return (
     <div className="relative w-full  select-none  ">
@@ -26,23 +29,39 @@ const YearDropdown = () => {
       </button>
 
       {isOpen && (
-        <ul
+        <motion.ul
+        initial={{ opacity: 0, y: -10 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: -10 }}
+        transition={{ duration: 0.2 }}
           ref={YearDropDownRef}
           className="absolute left-0 top-12 w-full bg-white/10 backdrop-blur-sm border border-zinc-700 rounded-lg shadow-md max-h-60 overflow-y-auto z-10 hide-scb divide-y divide-zinc-500/50 "
         >
-          {years.map((year) => (
-            <li
+          {years.map((year,index) => (
+            <motion.li
+            initial={{y: -10, opacity: 0, }}
+            animate={{ opacity: 1, y: 0, transition:{delay: 0.05 * index, duration: 0.2} }}
               key={year}
               className="px-4 py-2 hover:bg-white/5  cursor-pointer text-white select-none"
               onClick={() => handleSelect(year)}
             >
               {year}
-            </li>
+            </motion.li>
           ))}
-        </ul>
+          <p
+            onClick={() => {
+              setYear(0)
+              setSelectedYear('All years')
+              setIsOpen(false)
+            }}
+            className="px-4 py-2 hover:bg-white/5  cursor-pointer text-white select-none"
+          >
+            All years
+          </p>
+        </motion.ul>
       )}
     </div>
-  );
-};
+  )
+}
 
-export default YearDropdown;
+export default YearDropdown
--- src/renderer/src/screens/purchase/components/Searchbar.tsx
+++ src/renderer/src/screens/purchase/components/Searchbar.tsx
@@ -3,29 +3,36 @@ import { Input } from '../../../components/ui'
 import { fetchFilterPurchaseData } from '../service'
 import useDebounce from '@renderer/hooks/useDebounce'
 import { PurchaseDataType } from '@renderer/types/types'
-import { CiSearch } from "react-icons/ci";
+import { CiSearch } from 'react-icons/ci'
 import { fetchFilterSaleData } from '@renderer/screens/sale/service'
+import useYearFilterStore from '@renderer/state_manager/yearFilter'
 
 type SearchbarProps = {
   searchQuery: string
   setSearchQuery: React.Dispatch<React.SetStateAction<string>>
   setFilteredData: (data: PurchaseDataType[]) => void
-  type:string
+  type: string
 }
-const Searchbar: React.FC<SearchbarProps> = ({ searchQuery, setSearchQuery, setFilteredData , type}) => {
+const Searchbar: React.FC<SearchbarProps> = ({
+  searchQuery,
+  setSearchQuery,
+  setFilteredData,
+  type
+}) => {
   const debouncedValue = useDebounce(searchQuery, 500)
+  const { year } = useYearFilterStore()
   useEffect(() => {
     const ftnFetchFilterPurchaseData = async () => {
       if (!debouncedValue) return // Prevent unnecessary calls
       let response
       switch (type) {
-        case "sales":
-           response = await fetchFilterSaleData(debouncedValue) 
-          break;
-        case "purchase":
-           response = await fetchFilterPurchaseData(debouncedValue, "2025-02-15") 
+        case 'sales':
+          response = await fetchFilterSaleData(debouncedValue)
+          break
+        case 'purchase':
+          response = await fetchFilterPurchaseData(debouncedValue, JSON.stringify(year))
         default:
-          break;
+          break
       }
       setFilteredData(response)
       console.log(response)
@@ -34,8 +41,8 @@ const Searchbar: React.FC<SearchbarProps> = ({ searchQuery, setSearchQuery, setF
   }, [debouncedValue])
 
   return (
-    <div className='flex items-center gap-2 bg-zinc-700/20 rounded-full  ps-2 select-none'>
-      <CiSearch size={25} className='text-zinc-600' />
+    <div className="flex items-center gap-2 bg-zinc-700/20 rounded-full  ps-2 select-none">
+      <CiSearch size={25} className="text-zinc-600" />
       <Input
         style="bg-transparent  border-b border-transparent text-white"
         placeholder="Search Items"
